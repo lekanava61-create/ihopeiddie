8486a9dc827a8239d0e28ef2a77129ebd131a4ac
lekanava61-create <lekanava61@gmail.com>
2026-08-20 20:50:23 +0900

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://maven.aliucord.com/snapshots")
+    }
+}
+
+rootProject.name = "NukeAccount"
